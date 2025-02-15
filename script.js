45c292d375370dd89e08ceed97c22fb8cf82cc0a
rhap5ody-spotify <rhap5ody.spotify@gmail.com>
2025-02-15 13:48:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -22,28 +22,28 @@ let updateTimer;
 
 const music_list = [
     {
-        img : 'images/stay.png',
-        name : 'Stay',
-        artist : 'The Kid LAROI, Justin Bieber',
-        music : 'music/stay.mp3'
+        img : 'images/smalltown.png',
+        name : 'SMALL TOWN',
+        artist : 'Rhap5ody',
+        music : 'music/.mp3'
     },
     {
-        img : 'images/fallingdown.jpg',
-        name : 'Falling Down',
-        artist : 'Wid Cards',
-        music : 'music/fallingdown.mp3'
+        img : 'images/smalltown.png',
+        name : 'misfits.',
+        artist : 'Rhap5ody',
+        music : 'music/misfits.mp3'
     },
     {
-        img : 'images/faded.png',
-        name : 'Faded',
-        artist : 'Alan Walker',
-        music : 'music/Faded.mp3'
+        img : 'images/newcity.png',
+        name : 'NEW CITY',
+        artist : 'Rhap5ody',
+        music : 'music/.mp3'
     },
     {
-        img : 'images/ratherbe.jpg',
-        name : 'Rather Be',
-        artist : 'Clean Bandit',
-        music : 'music/Rather Be.mp3'
+        img : 'images/smalltown.png',
+        name : 'lost? (MINE)',
+        artist : 'Rhap5ody',
+        music : 'music/.mp3'
     }
 ];
 
