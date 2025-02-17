1c9c301ff0da9d92daacbc231a154a8b5a029891
rhap5ody-spotify <rhap5ody.spotify@gmail.com>
2025-02-17 17:00:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -68,7 +68,6 @@ function loadTrack(track_index){
 }
 
  function random_bg_color(){
-    let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
     let a;
 
     function populate(a){
